33f3ba5126d4fadde364025ed4a4a1c4adb02f98
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-05-01 23:35:32 +0700

    remove all unnecessary code inside ApplicationConfiguration class

--- src/main/java/com/assessment/ewallet/config/ApplicationConfiguration.java
+++ src/main/java/com/assessment/ewallet/config/ApplicationConfiguration.java
@@ -1,40 +1,9 @@
 package com.assessment.ewallet.config;
 
-import com.assessment.ewallet.repository.UserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+
+
 public class ApplicationConfiguration {
-    /*
-    private final UserRepository userRepository;
 
 
-    public ApplicationConfiguration(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
-    @Bean
-    UserDetailsService userDetailsService(){
-        return username -> userRepository.findByEmail()
-    }
-
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)  throws Exception{
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-     */
 }
