92d14d6986f6aa7b50cc773cc198f0a9b87e7555
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-05-03 19:51:19 +0700

    Add  Exception Handler to handle token auth exception

--- src/main/java/com/assessment/ewallet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/assessment/ewallet/exception/GlobalExceptionHandler.java
@@ -1,10 +1,53 @@
 package com.assessment.ewallet.exception;
 
+import com.assessment.ewallet.dto.ResponseDto;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({Exception.class})
+    @ResponseBody
+    public ResponseEntity<ResponseDto<String>> handleException(Exception exception) {
+        ResponseDto<String> responseDto = null;
+        ResponseEntity<ResponseDto<String>> responseEntity = null;
+
+        exception.printStackTrace();
+
+        if (exception instanceof BadCredentialsException) {
+            responseDto = new ResponseDto<>(103, "Username atau password salah", null);
+            responseEntity = new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
+        }
+        if (exception instanceof AccessDeniedException) {
+            responseDto = new ResponseDto<>(108, "Anda tidak memiliki hak akses", null);
+            responseEntity = new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
+        }
+        if (exception instanceof SignatureException) {
+            responseDto = new ResponseDto<>(108, "Token JWT Invalid", null);
+            responseEntity = new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
+        }
+        if (exception instanceof ExpiredJwtException) {
+            responseDto = new ResponseDto<>(108, "Token tidak valid atau kadaluwarsa", null);
+            responseEntity = new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
+        }
+
+        if (responseEntity == null) {
+            responseDto = new ResponseDto<>(103, "Internal server error.", null);
+            responseEntity = new ResponseEntity<>(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
+    }
 
 }
