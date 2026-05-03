2d50c54bfded16b71d7bf6dac36fc943bd966603
SYHRL360 <not.syahrul@outlook.co.id>
2026-05-04 00:28:41 +0700

    adding allow origins in cors configuration

--- src/main/java/com/assessment/ewallet/config/SecurityConfiguration.java
+++ src/main/java/com/assessment/ewallet/config/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://ewallet-production-f3cb.up.railway.app", "https://ewallet-production-f3cb.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
