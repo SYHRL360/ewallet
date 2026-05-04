858c273daecccc233b7b01e2be4b39e593291468
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-05-04 17:57:02 +0700

    adding localdate time util

--- src/main/java/com/assessment/ewallet/util/DateUtil.java
+++ src/main/java/com/assessment/ewallet/util/DateUtil.java
@@ -1,6 +1,9 @@
 package com.assessment.ewallet.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public final class DateUtil {
@@ -10,11 +13,25 @@ public final class DateUtil {
         return System.currentTimeMillis() / 1000;
     }
 
+
+
+    public static String getFmtDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(date);
+    }
+
     public static String getNowDateForTransaction(){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
         Date date = new Date();
         return sdf.format(date);
     }
+
+    public static LocalDateTime getLocalDateTime(){
+        ZoneId z = ZoneId.of("Asia/Ho_Chi_Minh");
+        return LocalDateTime.now(z);
+    }
+
+
 
 
 }
