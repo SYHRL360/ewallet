b68e324f80c533ad9499c4978b01fea2a3ed1b3f
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-05-05 10:47:31 +0700

    Modify CORS configuration

--- src/main/java/com/assessment/ewallet/config/SecurityConfiguration.java
+++ src/main/java/com/assessment/ewallet/config/SecurityConfiguration.java
@@ -57,7 +57,6 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
         configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://ewallet-production-f3cb.up.railway.app", "https://ewallet-production-f3cb.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
